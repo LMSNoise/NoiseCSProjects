090db6df5d771bab45d7fb5db2e379ee9a1f08a3
LMSNoise <114107545+LMSNoise@users.noreply.github.com>
2025-07-20 14:51:18 +0200

    Finished two more of the assignments honestly considering skipping some of these because I'm already familiar with OOP

---
+++ SistersDuckbear/Program.cs
@@ -0,0 +1,11 @@
+﻿int eggAmount;
+eggAmount = Convert.ToInt32(Console.ReadLine());
+
+int sisterAmount = eggAmount / 4;
+int bearAmount = eggAmount % 4;
+
+Console.WriteLine("Sister egg count: " + sisterAmount + "\nBear egg count: " + bearAmount);
+
+//For the bear to have more than the sister, you'd need to have a larger remainder than the number you'd get from division
+// that would be, then:
+//Any number below 4 (probably doesn't count here) or 6 (1 each, bear gets 2), 7 (1 each, bear gets 3), 11 (2 each, bear gets 3)
---
+++ TriangleFarmer/Program.cs
@@ -0,0 +1,12 @@
+﻿//calculate a triangle's area by base and height
+float baseLength;
+float heightLength;
+
+Console.WriteLine("What is the base length of the triangle?");
+baseLength = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("What is the height of the triangle?");
+heightLength = Convert.ToInt32(Console.ReadLine());
+
+float area = (baseLength * heightLength) / 2;
+Console.WriteLine("The triangle's area is: " + area);
